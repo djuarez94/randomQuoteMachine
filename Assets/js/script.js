0b6af27881696455f385461910b39278c5367d1f
David Juarez <DavidJuarez@Davids-MacBook-Pro-2.local>
2016-06-27 22:51:52 -0700

    worked on the nextQuote button functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,22 +6,45 @@ $(document).ready(function() {
   
   
   $('#nextQuote').on("click", function() {
-    nextRandomQuote();
+    randomQuote2();
   });
+
+  function randomQuote2 () {
+    var quotes = {
+          1: ['If a man does his best, what else is there?', 'Gen. George S. Patton'],
+          2: ['Give me chastity and continence, but not yet.', 'Saint Augustine'],
+          3: ['You can avoid reality, but you cannot avoid the consequences of avoiding reality.', 'Ayn Rand'],
+          4: ['I have always depended on the kindness of strangers.', 'A Streetcar Named Desire']
+        };
+        
+        
+        var randomQuoteNumber = Math.ceil(Math.random() * Object.keys(quotes).length);
+        
+        var randomColorChange = Math.ceil(Math.random() * Object.keys(colorChange).length);
+        
+        
+   
+        
+        $('.quoteBox').css("background-color", colorChange[randomColorChange]);
+
+        $('#randomQuote').text(quotes[randomQuoteNumber][0]);
+        $('#randomAuthor').text(quotes[randomQuoteNumber][1]);
+  }
+
   
-  function nextRandomQuote () {
-    var output = $.ajax({
-      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous',
-      type: 'GET', 
-      data: {}, 
-      dataType: 'json',
-      success: function(data) {
-        randomQuote = "\"" + data.quote + "\"";
-        randomAuthor = "- " +  data.author;
-        document.getElementById('randomQuote').innerHTML = randomQuote;
-        document.getElementById('randomAuthor').innerHTML = randomAuthor;
-      },
-      error: function(err) {
+  // function nextRandomQuote () {
+  //   var output = $.ajax({
+  //     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous',
+  //     type: 'GET', 
+  //     data: {}, 
+  //     dataType: 'json',
+  //     success: function(data) {
+  //       randomQuote = "\"" + data.quote + "\"";
+  //       randomAuthor = "- " +  data.author;
+  //       document.getElementById('randomQuote').innerHTML = randomQuote;
+  //       document.getElementById('randomAuthor').innerHTML = randomAuthor;
+  //     },
+  //     error: function(err) {
         
         // var quotes = {
         //   1: ['If a man does his best, what else is there?', 'Gen. George S. Patton'],
@@ -38,17 +61,17 @@ $(document).ready(function() {
         
    
         
-//         $('.quoteBox').css("background-color", colorSwitch[randomColorChange][0]);
+        // $('.quoteBox').css("background-color", colorSwitch[randomColorChange][0]);
 
-//         $('#quote').text(quotes[randomQuoteNumber][0]);
-//         $('#author').text(quotes[randomQuoteNumber][1]);
+        // $('#randomQuote').text(quotes[randomQuoteNumber][0]);
+        // $('#randomAuthor').text(quotes[randomQuoteNumber][1]);
 
-      },
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader("X-Mashape-Authorization", "Sr4ugfUfUHmshOQ2IbeGXw8i1Gujp15JgPZjsnIcl0TaxHmdX3");
-      }
-    });
-  }
+  //     },
+  //     beforeSend: function(xhr) {
+  //       xhr.setRequestHeader("X-Mashape-Authorization", "Sr4ugfUfUHmshOQ2IbeGXw8i1Gujp15JgPZjsnIcl0TaxHmdX3");
+  //     }
+  //   });
+  // }
   
   function colorChange () {
     var randomColors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
@@ -127,7 +150,7 @@ $('#twitterShare').on("click", function() {
     {
     method: 'feed',
     name: 'Random Quote Machine',
-    link: 'http://codepen.io/djuarez/pen/XdBdgW',
+    link: 'https://djuarez94.github.io/randomQuoteMachine/',
     picture: 'http://www.groupstudy.in/img/logo3.jpeg',
     caption: 'Generate random famous quotes from famous movies and people.',
     description: "Want to view a random famous quote and share it with your friends and family? Try out my Random Quote Machine!",
